c6564546e28d9cca6003ec9b38d0f2466329df76
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-11-13 13:07:58 -0500

    react-hooks-lecture-videos-forms
    react-hooks-lecture-videos-forms

---
+++ index.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+// - React Forms: https://reactjs.org/docs/forms.html
+
+function SignUp() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [accountType, setAccountType] = useState("Free");
+  const [newsletter, setNewsletter] = useState(true);
+  
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const formData = { 
+    username,
+    password,
+    avatar,
+    accountType,
+    newsletter
+  }
+  console.log(formData);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Create an Account</h1>
+      <label htmlFor="username">Username</label>
+      <input
+        type="text"
+        id="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        id="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <label htmlFor="avatar">Avatar Image</label>
+      <input
+        type="text"
+        id="avatar"
+        value={avatar}
+        onChange={(e) => setAvatar(e.target.value)}
+      />
+      <img
+        src={avatar}
+        alt="Avatar preview"
+      />
+
+      <label htmlFor="type">Account Type</label>
+      <select
+  id="type"
+  value={accountType}
+  onChange={(e) => setAccountType(e.target.value)}
+>
+        <option value="free">Free</option>
+        <option value="normal">Normal</option>
+        <option value="pro">Pro</option>
+      </select>
+
+      <label>
+        Get Our Newsletter!
+        <input
+          type="checkbox"
+          id="newsletter"{newsletter}
+          value={newsletter}
+          onChange={(e) => setNewsletter(e.target.checked)}
+        />
+      </label>
+
+      <input type="submit" value="Sign Up" />
+    </form>
+  );
+}
+
+ReactDOM.render(<SignUp />, document.getElementById('root'));
